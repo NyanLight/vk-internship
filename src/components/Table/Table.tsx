6b6e82c796465cf173c78990ed3d330015332e0f
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-05-31 21:55:40 +0400

    Add async to Table component and some basic style to it

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,29 +1,67 @@
 import styles from "./Table.module.css";
+import { useEffect, useState } from "react";
 
 interface Entry {
   id: number;
 }
 
-export function Table({ entries }: { entries: Entry[] }) {
-  const headings = Object.keys(entries[0]).filter((key) => key !== "id");
+export function Table({ url }: { url: string }) {
+  const [entries, setEntries] = useState<null | Entry[]>(null);
+  const [error, setError] = useState<null | string>(null);
+  const [loading, setLoading] = useState(true);
 
-  return (
-    <table className={styles.table}>
-      <tr className={styles.row}>
-        {headings.map((heading) => {
-          return <th className={styles.heading}>{heading}</th>;
+  useEffect(() => {
+    async function getEntries() {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        setEntries(json);
+        setLoading(false);
+        console.log(response.status);
+      } catch (error) {
+        let errorMessage = "Failed to do something exceptional";
+        if (error instanceof Error) {
+          errorMessage = error.message;
+        }
+        setError(errorMessage);
+        setLoading(false);
+      }
+    }
+    getEntries();
+  }, [url]);
+
+  if (error) return <div>Error: {error}!</div>;
+
+  if (loading) return <div>Loading...</div>;
+
+  if (entries) {
+    const headings = Object.keys(entries[0]).filter((key) => key !== "id");
+
+    return (
+      <table className={styles.table}>
+        <tr className={styles.row}>
+          {headings.map((heading) => {
+            return (
+              <th scope="col" className={styles.heading}>
+                {heading}
+              </th>
+            );
+          })}
+        </tr>
+        {entries.map((entry) => {
+          return (
+            <tr className={styles.row}>
+              {Object.entries(entry).map((field) => {
+                if (field[0] !== "id")
+                  return <td className={styles.data}>{field[1]}</td>;
+              })}
+            </tr>
+          );
         })}
-      </tr>
-      {entries.map((entry) => {
-        return (
-          <tr>
-            {Object.entries(entry).map((field) => {
-              if (field[0] !== "id")
-                return <td className={styles.data}>{field[1]}</td>;
-            })}
-          </tr>
-        );
-      })}
-    </table>
-  );
+      </table>
+    );
+  }
 }
