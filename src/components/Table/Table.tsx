e1e93be26d32c2a153f3811875da1742ff227761
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-06-01 21:36:41 +0400

    Rewrite Table component to use hooks and mobx store

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,94 +1,66 @@
 import styles from "./Table.module.css";
-import { useEffect, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { observer } from "mobx-react-lite";
+import { entriesStore } from "../../EntriesStore";
+import { useFetchEntries } from "../../hooks/useFetchEntries";
+import { useInfiniteScroll } from "../../hooks/useInfiniteScroll";
 
-interface Entry {
-  id: number;
-}
-
-export function Table({ url }: { url: string }) {
-  const [entries, setEntries] = useState<null | Entry[]>(null);
-  const [error, setError] = useState<null | string>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [next, setNext] = useState<null | string>(null);
-  const [hasMore, setHasMore] = useState<boolean>(true);
-
-  const fetchData = async (url: string) => {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-      const json = await response.json();
-      if (entries === null) {
-        setEntries(json.games);
-      } else {
-        setEntries([...entries, ...json.games]);
-      }
-      if (json.next !== null) {
-        setNext(json.next);
-      } else {
-        setHasMore(false);
-      }
-      setLoading(false);
-    } catch (error) {
-      let errorMessage = "Failed to do something exceptional";
-      if (error instanceof Error) {
-        errorMessage = error.message;
-      }
-      setError(errorMessage);
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchData(url);
-  }, []);
+export const Table = observer(({ url }: { url: string }) => {
+  const { error, loading, next, hasMore, fetchData } = useFetchEntries(url);
+  const {
+    nextHandler,
+    dataLength,
+    hasMore: scrollHasMore,
+    loader,
+  } = useInfiniteScroll({
+    next,
+    hasMore,
+    fetchData,
+  });
 
   if (error) return <div>Error: {error}!</div>;
 
   if (loading) return <div>Loading...</div>;
 
-  if (entries) {
-    const headings = Object.keys(entries[0])
+  if (entriesStore.entries) {
+    const headings = Object.keys(entriesStore.entries[0])
       .filter((key) => key !== "id")
       .slice(0, 15);
 
     return (
       <InfiniteScroll
-        next={() => {
-          if (typeof next === "string") fetchData(next);
-        }}
-        dataLength={entries.length}
-        hasMore={hasMore}
-        loader={<div>Loading...</div>}
+        next={nextHandler}
+        dataLength={dataLength}
+        hasMore={scrollHasMore}
+        loader={loader}
       >
         <table className={styles.table}>
           <thead>
             <tr className={styles.row}>
-              {headings.map((heading) => {
-                return (
-                  <th scope="col" className={styles.heading}>
-                    {heading}
-                  </th>
-                );
-              })}
+              {headings.map((heading) => (
+                <th scope="col" className={styles.heading} key={heading}>
+                  {heading}
+                </th>
+              ))}
             </tr>
           </thead>
           <tbody>
-            {entries.map((entry) => {
-              return (
-                <tr className={styles.row}>
-                  {Object.entries(entry).map((field) => {
-                    if (headings.includes(field[0]))
-                      return <td className={styles.data}>{field[1]}</td>;
-                  })}
-                </tr>
-              );
-            })}
+            {entriesStore.entries.map((entry) => (
+              <tr className={styles.row} key={entry.id}>
+                {Object.entries(entry).map((field) => {
+                  if (headings.includes(field[0]))
+                    return (
+                      <td className={styles.data} key={field[0]}>
+                        {field[1]}
+                      </td>
+                    );
+                })}
+              </tr>
+            ))}
           </tbody>
         </table>
       </InfiniteScroll>
     );
   }
-}
+  return null;
+});
